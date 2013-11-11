096c5d8f19b4a94792c3964ac712b9ae5ff7d0d0
Johan Stenehall <stenehall@gmail.com>
2013-11-11 20:31:27 +0100

    Cleaning up nock data

--- test/igelkott-bitcoin_test.js
+++ test/igelkott-bitcoin_test.js
@@ -35,9 +35,9 @@ describe('Bitcoin', function() {
       }
     });
 
-    var couchdb = nock('http://data.mtgox.com')
-                .get('/api/2/BTCUSD/money/ticker_fast')
-                .reply(200, {"result":"success","data":{"last_local":{"value":"371.42000","value_int":"37142000","display":"$371.42","display_short":"$371.42","currency":"USD"},"last":{"value":"371.42000","value_int":"37142000","display":"$371.42","display_short":"$371.42","currency":"USD"},"last_orig":{"value":"271.55600","value_int":"27155600","display":"271.56\u00a0\u20ac","display_short":"271.56\u00a0\u20ac","currency":"EUR"},"last_all":{"value":"364.72686","value_int":"36472686","display":"$364.73","display_short":"$364.73","currency":"USD"},"buy":{"value":"371.42000","value_int":"37142000","display":"$371.42","display_short":"$371.42","currency":"USD"},"sell":{"value":"371.49643","value_int":"37149643","display":"$371.50","display_short":"$371.50","currency":"USD"},"now":"1384194990833249"}});
+    nock('http://data.mtgox.com')
+      .get('/api/2/BTCUSD/money/ticker_fast')
+      .reply(200, {"result":"success","data":{"buy":{"value":"371.42000"}}});
 
     igelkott.connect();
     s.write(":dsmith!~dsmith@unaffiliated/dsmith PRIVMSG ##botbotbot :!bitcoin\r\n");
@@ -54,9 +54,9 @@ describe('Bitcoin', function() {
       }
     });
 
-    var couchdb = nock('http://data.mtgox.com')
-                .get('/api/2/BTCSEK/money/ticker_fast')
-                .reply(200, {"result":"success","data":{"last_local":{"value":"2245.520","value_int":"2245520","display":"2,246\u00a0Kr","display_short":"2,246\u00a0Kr","currency":"SEK"},"last":{"value":"2245.520","value_int":"2245520","display":"2,246\u00a0Kr","display_short":"2,246\u00a0Kr","currency":"SEK"},"last_orig":{"value":"371.42000","value_int":"37142000","display":"$371.42","display_short":"$371.42","currency":"USD"},"last_all":{"value":"2443.916","value_int":"2443916","display":"2,444\u00a0Kr","display_short":"2,444\u00a0Kr","currency":"SEK"},"buy":{"value":"2384.309","value_int":"2384309","display":"2,384\u00a0Kr","display_short":"2,384\u00a0Kr","currency":"SEK"},"sell":{"value":"2444.419","value_int":"2444419","display":"2,444\u00a0Kr","display_short":"2,444\u00a0Kr","currency":"SEK"},"now":"1384195209157671"}});
+    nock('http://data.mtgox.com')
+      .get('/api/2/BTCSEK/money/ticker_fast')
+      .reply(200, {"result":"success","data":{"buy":{"value":"2384.309"}}});
 
     igelkott.connect();
     s.write(":dsmith!~dsmith@unaffiliated/dsmith PRIVMSG ##botbotbot :!bitcoin SEK\r\n");
